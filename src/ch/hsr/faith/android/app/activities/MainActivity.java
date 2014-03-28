b8dc89ec075434cce54b293b93d1f396b010aa6f
hsisic <hsisic@hsr.ch>
2014-03-28 12:13:06 +0100

    Implemented MainActivity UI for Prototype

--- src/ch/hsr/faith/android/app/activities/MainActivity.java
+++ src/ch/hsr/faith/android/app/activities/MainActivity.java
@@ -1,10 +1,16 @@
 package ch.hsr.faith.android.app.activities;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 import ch.hsr.faith.android.app.R;
+import ch.hsr.faith.android.app.activities.adapters.ExpandableListAdapter;
 import ch.hsr.faith.android.app.domain.FurnitureCategory;
 import ch.hsr.faith.android.app.domain.FurnitureCategoryList;
 import ch.hsr.faith.android.app.services.FurnitureCategoriesRootRequest;
@@ -21,6 +27,11 @@ public class MainActivity extends Activity {
 	private TextView testServiceResponseTextView;
 	private String lastFurnitureCategoriesRootRequestCacheKey;
 
+	ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    ArrayList<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
+    
 	@Override
 	protected void onStart() {
 		super.onStart();
@@ -32,14 +43,27 @@ public class MainActivity extends Activity {
 		spiceManager.shouldStop();
 		super.onStop();
 	}
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		testServiceResponseTextView = (TextView) findViewById(R.id.TestServiceResponseView);
+//		testServiceResponseTextView = (TextView) findViewById(R.id.TestServiceResponseView);
 
+		// get the listview
+        expListView = (ExpandableListView) findViewById(R.id.lvExp);
+        
+		listDataHeader = new ArrayList<String>();
+		listDataChild = new HashMap<String, List<String>>();
 		loadFurnitureCategories();
+		
+ 
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+	
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
 	}
 
 	@Override
@@ -66,12 +90,19 @@ public class MainActivity extends Activity {
 		}
 
 		public void onRequestSuccess(FurnitureCategoryList furnitureCategoryList) {
-			testServiceResponseTextView.setText("\n\nCategories:\n");
-			for (FurnitureCategory furnitureCategory : furnitureCategoryList) {
-				testServiceResponseTextView.append(furnitureCategory.getName() + "\n");
-			}
+//			testServiceResponseTextView.setText("\n\nCategories:\n");
+//			for (FurnitureCategory furnitureCategory : furnitureCategoryList) {
+//				testServiceResponseTextView.append(furnitureCategory.getName() + "\n");
+//			}
+//			FurnitureCategoryList categories = request.loadDataFromNetwork();
+			
+	    	for (FurnitureCategory s : furnitureCategoryList){
+	    		listDataHeader.add(s.getName());
+	    	}
+	    	listAdapter.notifyDataSetChanged();
 		}
 
 	}
+    
 
 }
--- src/ch/hsr/faith/android/app/services/FurnitureCategoriesRootRequest.java
+++ src/ch/hsr/faith/android/app/services/FurnitureCategoriesRootRequest.java
@@ -1,5 +1,7 @@
 package ch.hsr.faith.android.app.services;
 
+import org.springframework.web.client.RestClientException;
+
 import ch.hsr.faith.android.app.domain.FurnitureCategoryList;
 
 import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
@@ -11,9 +13,16 @@ public class FurnitureCategoriesRootRequest extends SpringAndroidSpiceRequest<Fu
 	}
 
 	@Override
-	public FurnitureCategoryList loadDataFromNetwork() throws Exception {
+	public FurnitureCategoryList loadDataFromNetwork()  {
 		String url = "http://sinv-56078.edu.hsr.ch/faith-rest-application/furniture-categories/all-roots";
-		return getRestTemplate().getForObject(url, FurnitureCategoryList.class);
+		FurnitureCategoryList result;
+		try {
+			result =  getRestTemplate().getForObject(url, FurnitureCategoryList.class);			
+		} catch (RestClientException e) {
+			System.err.println("communication error");
+			result = new FurnitureCategoryList();
+		}
+		return result;
 	}
 
 	public String createCacheKey() {
