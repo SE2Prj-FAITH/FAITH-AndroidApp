b9f089c90cd53c333974877e870f6bf0e6cd3e6f
Daniel Kolb <kolbdaniel1@gmail.com>
2014-04-10 11:40:44 +0200

    fix localizing style bug

--- src/ch/hsr/faith/android/app/activities/MainActivity.java
+++ src/ch/hsr/faith/android/app/activities/MainActivity.java
@@ -4,10 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.ExpandableListView;
 import ch.hsr.faith.android.app.R;
 import ch.hsr.faith.android.app.activities.adapters.ExpandableListAdapter;
@@ -35,6 +32,9 @@ public class MainActivity extends BaseActivity {
 		loadSystemProperties();
 	}
 
+	
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
