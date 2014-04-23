5d5ee0f6d724380e1adb9ea87ed7863f798ec9ef
Stefan Kapferer <stefan@kapferer.ch>
2014-04-23 21:20:06 +0200

    Implemented request to get facilities by category

---
+++ src/ch/hsr/faith/android/app/dto/FacilityList.java
@@ -0,0 +1,11 @@
+package ch.hsr.faith.android.app.dto;
+
+import java.util.ArrayList;
+
+import ch.hsr.faith.domain.Facility;
+
+public class FacilityList extends ArrayList<Facility> {
+
+	private static final long serialVersionUID = 8319415648845463607L;
+
+}
